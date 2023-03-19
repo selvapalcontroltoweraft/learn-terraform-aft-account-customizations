bec37369efbfa86a24a1efe14123aa04aace2e6b
selvapalcontroltoweraft <128287757+selvapalcontroltoweraft@users.noreply.github.com>
2023-03-19 23:51:36 +1100

    acct customizations

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,3 +4,8 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "sandbox_bucket" {
+  bucket = "aft-sandbox-acctcust-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
