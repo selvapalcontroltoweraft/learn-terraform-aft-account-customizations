e609a3309a88fcbe7ab50a83b1fea84385ae14b6
Selvakumar Palanisamy <selvakumar.palanisamy@sourcedgroup.com>
2023-03-20 18:39:24 +1100

    acct customizations

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -9,3 +9,8 @@ resource "aws_s3_bucket" "sandbox_bucket1" {
   bucket = "aft-sandbox-acctcust-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "sandbox_microservice" {
+  bucket = "aft-sandbox-acctms-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
